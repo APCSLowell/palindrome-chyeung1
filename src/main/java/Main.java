404890548a1fdf8405167abd4379e14cacb8f21a
chyeung1 <143123817+chyeung1@users.noreply.github.com>
2024-01-23 19:33:19 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,21 @@ public class Main {
 	public static void main(final String[] args) {
 		PalindromeChecker sim = new PalindromeChecker();
         	sim.tester();
+		palindrome("sdfds");
+	}
+	public String reverse(String sWord)
+	{
+  	String reverseString = "";
+  	for(int i = sWord.length(); i > 0; i--)
+ 	 reverseString += sWord.substring(i - 1, i);  
+ 	 return reverseString;
 	}
 	
-
+	public boolean pallindrome(String sWord)
+	{
+  	if(reverse(sWord).equals(sWord))
+   	 return true;
+   	 return false;
+	}
 }
 
