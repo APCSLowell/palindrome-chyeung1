6084339712865206045d2bf2ce8c74b6d92847e6
chyeung1 <143123817+chyeung1@users.noreply.github.com>
2024-01-27 23:55:31 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,21 +5,6 @@ public class Main {
 	public static void main(final String[] args) {
 		PalindromeChecker sim = new PalindromeChecker();
         	sim.tester();
-		palindrome("bruh");
-	}
-	public String reverse(String sWord)
-	{
-  	String reverseString = "";
-  	for(int i = sWord.length(); i > 0; i--)
- 	 reverseString += sWord.substring(i - 1, i);  
- 	 return reverseString;
-	}
-	
-	public boolean pallindrome(String sWord)
-	{
-  	if(reverse(sWord).equals(sWord))
-   	 return true;
-   	 return false;
 	}
 }
 
